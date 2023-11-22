ae88e285b50ea7b0c96de95972577d80ad240db5
KOREAVC <KOREAVC@DESKTOP-OO4UAMB>
2023-11-22 17:37:27 +0900

    11월22일

--- jsp1117/src/main/java/com/java/www/service/BReplyService.java
+++ jsp1117/src/main/java/com/java/www/service/BReplyService.java
@@ -6,21 +6,22 @@ import javax.servlet.http.HttpServletResponse;
 import com.java.www.dao.BoardDao;
 import com.java.www.dto.BoardDto;
 
-public class BSelectOneService implements Service {
+public class BReplyService implements Service {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
-		//dao접근
+		//전송된 데이터
 		int bno = Integer.parseInt(request.getParameter("bno"));
+		
+		//dao접근
 		BoardDao bdao = new BoardDao();
-		//조회수 1증가
-		bdao.bHitUp(bno);
-		//1개 게시글 가져오기
 		BoardDto bdto = bdao.selectOne(bno);
 		
-		
-		//request
+		//request 추가
 		request.setAttribute("bdto", bdto);
+		
+		
+
 	}
 
 }
